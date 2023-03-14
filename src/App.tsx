ef8e7c9307b1a895bbc2eedb756a4d3ed540b451
yhlee <kamsyyongho@naver.com>
2023-03-14 17:19:33 +0900

    add component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,35 @@
+import React, {Component} from 'react'
 import * as D from './data'
 
-function App() {
-  const children = D.makeArray(10).map((notUesed, index) => (
-    <div key={index}>
-      <p>{D.randomId()}</p>
-      <p>{D.randomName()}</p>
-      <p>{D.randomJobTitle()}</p>
-      <p>{D.randomSentence()}</p>
-      <img src={D.randomAvatar()} width={100} height={100}></img>
-    </div>
-  ))
+export default class App extends Component{
+  render() {
+    const isLoading = false
+    const children = isLoading?<p>loading...</p>
+      :<ul>
+        <li>
+          <a href='http://www.google.com'>
+            <p>go to Google</p>
+          </a>
+        </li>
+      </ul>
+    return(
+      <div>{children}</div>
+    )
+  }
+}
 
-  return <div>{children}</div>
+// function App() {
+//   const children = D.makeArray(10).map((notUesed, index) => (
+//     <div key={index}>
+//       <p>{D.randomId()}</p>
+//       <p>{D.randomName()}</p>
+//       <p>{D.randomJobTitle()}</p>
+//       <p>{D.randomSentence()}</p>
+//       <img src={D.randomAvatar()} width={100} height={100}></img>
+//     </div>
+//   ))
+
+//   return <div>{children}</div>
 
   // return (
   //   <div>
@@ -22,6 +40,6 @@ function App() {
   //     <img src={D.randomImage()} height="300" />
   //   </div>
   // )
-}
+// }
 
-export default App
+// export default App
