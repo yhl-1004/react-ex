0c0be5abd76f7379dada2569368e2826ed64d421
kamsyyongho@naver.com <kamsyyongho@naver.com>
2023-04-13 18:20:10 +0900

    add useReducer hook

--- src/pages/UseReducerClock.tsx
+++ src/pages/UseReducerClock.tsx
@@ -1,3 +1,31 @@
+import {useSelector, useDispatch} from 'react-redux'
+import {Div} from '../components'
+import type {AppState} from '../store'
+import {useInterval} from '../hooks'
+import {useReducer} from 'react'
+import {SetTodayAction} from '../store/actions'
+
 export default function UseReducerClock() {
-  return <></>
+  const [{today}, dispatch] = useReducer(
+    (state: AppState, action: SetTodayAction) => {
+      switch (action.type) {
+        case 'setToday':
+          return {...state, today: new Date()}
+      }
+    },
+    {today: new Date()}
+  )
+
+  useInterval(() => {
+    console.log(new Date(), 'ReduxClock')
+    dispatch({type: 'setToday', today: new Date()})
+  })
+
+  return (
+    <Div className="">
+      <Div>ReduxClock</Div>
+      <Div>{today.toLocaleTimeString()}</Div>
+      <Div>{today.toLocaleDateString()}</Div>
+    </Div>
+  )
 }
